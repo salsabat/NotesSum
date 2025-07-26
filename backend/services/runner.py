e5d96ec2577f01a0767c78957668525f3bdd1947
Sajal Sabat <sajalsabat@gmail.com>
2025-07-26 05:52:28 -0400

    impl manual retrieval

--- backend/services/runner.py
+++ backend/services/runner.py
@@ -1,13 +1,12 @@
-from langchain_openai import ChatOpenAI
-from langchain.chains import RetrievalQA
-from langchain.vectorstores import Pinecone as LangchainPinecone
-from langchain_openai import OpenAIEmbeddings
-
-from embedder import Embedder
-from PineconeDB import PCDB
-
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from PineconeDB import PCDB
+from embedder import Embedder
+from langchain_core.documents import Document
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain import hub
+
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
@@ -15,31 +14,44 @@ PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 
 
 class RAGRunner:
-
     def __init__(self, index_name, model_name="gpt-4.1-nano", temperature=0.3):
         self.index_name = index_name
+
         self.pcdb = PCDB(index_name=index_name)
         self.embedder = Embedder(pcdb_instance=self.pcdb)
-
-        self.embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-        self.retriever = LangchainPinecone.from_existing_index(
-            self.index_name, self.embeddings).as_retriever()
 
         self.llm = ChatOpenAI(
             model=model_name,
             temperature=temperature,
             openai_api_key=OPENAI_API_KEY
         )
-
-        self.qa_chain = RetrievalQA.from_chain_type(
-            llm=self.llm,
-            retriever=self.retriever,
-            return_source_documents=False
-        )
+        self.retrieval_qa_chat_prompt = hub.pull(
+            "langchain-ai/retrieval-qa-chat")
+        self.combine_chain = create_stuff_documents_chain(
+            llm=self.llm, prompt=self.retrieval_qa_chat_prompt)
 
     def ingest(self, text, category):
         vectors = self.embedder.embed_document(text, category=category)
         return self.pcdb.insert_index(vectors)
 
-    def ask(self, question):
-        return self.qa_chain.run(question)
+    def ask(self, question, top_k=10, category=None):
+        if category:
+            texts = self.embedder.search_texts_by_query(
+                query=question, top_k=top_k, filter={'category': category})
+        else:
+            texts = self.embedder.search_texts_by_query(
+                query=question, top_k=top_k)
+
+        matches = texts.get('matches', [])
+        docs = [
+            Document(page_content=match['metadata']
+                     ['text'], metadata=match['metadata'])
+            for match in matches
+        ]
+
+        response = self.combine_chain.invoke({
+            "input": question,
+            "context": docs
+        })
+
+        return response['answer']
